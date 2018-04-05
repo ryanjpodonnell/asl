5cea4871bf0786b0e301e261cb2b5cd8c4fe8f32
Ryan O'Donnell <ryan.jp.odonnell@gmail.com>
2018-04-04 19:10:45 -0700

    Use JQuery to only show one section at a time; Remove H1

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,21 @@
 $(document).ready(function() {
   $("#trigger-about").click(function() {
-    $('#content').childen().hide();
-    $('#about').unhide();
+    $('#content').children().hide();
+    $('#about').show();
+  });
+
+  $("#trigger-venues").click(function() {
+    $('#content').children().hide();
+    $('#venues').show();
+  });
+
+  $("#trigger-rules").click(function() {
+    $('#content').children().hide();
+    $('#rules').show();
+  });
+
+  $("#trigger-links").click(function() {
+    $('#content').children().hide();
+    $('#links').show();
   });
 });
